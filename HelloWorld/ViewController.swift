1e168c7286fe8258d6c5c9fcdedf655ef1282bc3
Lydia Nash <lydiamnash@gmail.com>
2018-07-20 13:31:45 -0500

    Initial commit of HelloWorld app

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Lydia Nash on 7/20/18.
+//  Copyright © 2018 Lydia Nash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to my first App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
